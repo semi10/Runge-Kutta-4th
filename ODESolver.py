c698761ac12a0f3324dac258d699439509b27e9d
FSDragon <f.semion89@gmail.com>
2016-03-03 01:47:55 +0200

    added graph draw capability

--- ODESolver.py
+++ ODESolver.py
@@ -1,59 +1,58 @@
-from numpy import zeros, asarray
-from scitools.std import pi, linspace
+from scitools.std import pi
 
-class ODESolver:
-    def __init__(self, u_init, m, beta, k, g): 
-        u_init = asarray(u_init)
-        self.u_init, self.m, self.beta, self.k, self.g = u_init, float(m), float(beta), float(k), float(g)
+class RungeKutta4th:
+    def __init__(self, s_init, v_init, m, beta, k, g): 
+        self.s_init, self.v_init, self.m, self.beta, self.k, self.g = float(s_init) ,float(v_init), float(m), float(beta), float(k), float(g)
         
-    def f(self, u):
-        u0, u1 = u
-        f = asarray([u1, 1./self.m*(self.g - self.beta*u1 - self.k*u0)])
-        return f
+    def f_s(self, u1):
+        return u1
+    
+    def f_v(self, u0, u1):
+        return 1./self.m*(self.g - self.beta*u1 - self.k*u0)
+    
 
-    def solve(self, time_points):
-        """Compute u for t values in time_points list."""
-        self.t = asarray(time_points)
-        n = self.t.size
-        self.u = zeros((n, 2))
-        # Assume self.t[0] corresponds to self.U0
-        self.u[0] = self.u_init
-
+    def solve(self, total_time, dt):
+        f_s = self.f_s
+        f_v = self.f_v
+        
+        N = int(total_time/dt)   #N - Steps to make
+        S = [0.0]*(N+1)          #S - Empty list of spring stretch
+        V = [0.0]*(N+1)          #V - List of spring velocity
+        T = [0.0]*(N+1)          #T - Time points
+        
+        S[0] = self.s_init
+        V[0] = self.v_init
+                
         #Time loop
-        for i in range(n-1):
-            self.i = i
-            self.u[i+1] = self.advance()
-        return self.u, self.t
-
-    def advance(self):
-        u, f, i, t = self.u, self.f, self.i, self.t
-        dt = t[i+1] - t[i]
-        K1 = dt*f(u[i])
-        K2 = dt*f(u[i] + 0.5*K1)
-        K3 = dt*f(u[i] + 0.5*K2)
-        K4 = dt*f(u[i] + K3)
-        u_new = u[i] + (1/6.0)*(K1 + 2*K2 + 2*K3 + K4)
-        return u_new
+        for i in range(N):
+            k1_s = dt*f_s(V[i])
+            k1_v = dt*f_v(S[i], V[i])
+            
+            k2_s = dt*f_s(V[i]+0.5*k1_v)
+            k2_v = dt*f_v(S[i]+0.5*k1_s, V[i]+0.5*k1_v)
+            
+            k3_s = dt*f_s(V[i]+0.5*k2_v)
+            k3_v = dt*f_v(S[i]+0.5*k2_s, V[i]+0.5*k2_v)
+            
+            k4_s = dt*f_s(V[i]+k3_v)
+            k4_v = dt*f_v(S[i]+k3_s, V[i]+k3_v)
+            
+            S[i+1] = S[i] + (1/6.0)*(k1_s + 2*k2_s + 2*k3_s + k4_s)
+            V[i+1] = V[i] + (1/6.0)*(k1_v + 2*k2_v + 2*k3_v + k4_v)
+            T[i+1] = dt*(i+1)
+        return S, V, T
+        
 
 # Test case: u = cos(t) 
 if __name__ == '__main__':
-    u_init = [1, 0]    # initial condition
-    solver = ODESolver(u_init, m=1, beta=0, k=1, g=0)
+    solver = RungeKutta4th(s_init=1, v_init=0, m=1, beta=0, k=1, g=0)
     
     nperiods = 3     # no of oscillation periods
-    T = 2*pi*nperiods
-    npoints_per_period = 20
-    n = npoints_per_period*nperiods
-    t_points = linspace(0, T, n+1)
+    total_time = 2*pi*nperiods
+    dt = pi/10
     
-    u, t = solver.solve(t_points)
+    S, V, T = solver.solve(total_time, dt)
     
-    # u is an array of [u0,u1] pairs for each time level,
-    # get the u0 values from u for plotting
-    u0_values = u[:, 0]
-    u1_values = u[:, 1]
-    
-    print '    Second:     \tPosition:\t     Velocity:'
-    for i,j,k in zip(t,u0_values, u1_values):
-        print '%-19.15f %19.15f %19.15f' % (i,j, k)
-    
+    print 'Time(sec):   \t\tPosition(m):\t Velocity(m/sec):'
+    for i, j, k in zip(T, S, V):
+        print '%18.15f %18.15f %19.15f' % (i, j, k)   
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Tkinter import *
 from  ODESolver import *
-from numpy import zeros, asarray
-from scitools.std import pi, linspace
+import matplotlib.pyplot as plt
+from scitools.std import *
+
 
 class Menu:
     def __init__(self, parent):
@@ -9,77 +10,147 @@ class Menu:
         self.beta = DoubleVar()
         self.k = DoubleVar()
         self.g = DoubleVar()
-        self.u0 = DoubleVar() 
-        self.u1 = DoubleVar()
-        self.m.set(1.)
-        self.beta.set(0.)
-        self.k.set(1.)
-        self.g.set(0.)
-        self.u0.set(1.)
-        self.u1.set(0.)
+        self.s_init = DoubleVar() 
+        self.v_init = DoubleVar()
+        self.total_time = DoubleVar()
+        self.dt = DoubleVar()
         
         self.label_1 = Label(parent, text="Mass:")
         self.label_1.grid(row=0, column=0)
         self.entry_1 = Entry(parent, width=8, textvariable=self.m)
-        self.entry_1.grid(row=1, column=0)
+        self.entry_1.grid(row=1, column=0, sticky=N)
         
         self.label_2 = Label(parent, text="Damping:")
         self.label_2.grid(row=0, column=1)
         self.entry_2 = Entry(parent, width=8, textvariable=self.beta)
-        self.entry_2.grid(row=1, column=1)  
+        self.entry_2.grid(row=1, column=1, sticky=N)  
         
         self.label_3 = Label(parent, text="Spring:")
         self.label_3.grid(row=0, column=2)
         self.entry_3 = Entry(parent, width=8, textvariable=self.k)
-        self.entry_3.grid(row=1, column=2)
+        self.entry_3.grid(row=1, column=2, sticky=N)
         
         self.label_4 = Label(parent, text="Gravity:")
         self.label_4.grid(row=0, column=3)
         self.entry_4 = Entry(parent, width=8, textvariable=self.g)
-        self.entry_4.grid(row=1, column=3)
-        
-        self.calculate_bt = Button(parent, text="Calculate", command=self.calculate)
-        self.calculate_bt.grid(row=2, column=4, padx=20)
+        self.entry_4.grid(row=1, column=3, sticky=N)
         
         self.label_5 = Label(parent, text="Initial Conditions:")
-        self.label_5.grid(columnspan=4,row=2,column=0, sticky=S)
+        self.label_5.grid(columnspan=4,row=3,column=0, sticky=S)
 
         self.label_6 = Label(parent, text="Stretch:")
-        self.label_6.grid(row=3,column=0)
-        self.entry_5 = Entry(parent, width=8, textvariable=self.u0)
-        self.entry_5.grid(row=3, column=1)
+        self.label_6.grid(row=4,column=0, sticky=E)
+        self.entry_5 = Entry(parent, width=8, textvariable=self.s_init)
+        self.entry_5.grid(row=4, column=1)
         
         self.label_7 = Label(parent, text="Velocity:")
-        self.label_7.grid(row=3,column=2)
-        self.entry_6 = Entry(parent, width=8, textvariable=self.u1)
-        self.entry_6.grid(row=3, column=3)
+        self.label_7.grid(row=4,column=2, sticky=E)
+        self.entry_6 = Entry(parent, width=8, textvariable=self.v_init)
+        self.entry_6.grid(row=4, column=3)
+        
+        self.label_8 = Label(parent, text ="Time parameters:")
+        self.label_8.grid(columnspan=4, row=6, column=0, sticky=S)
+        
+        self.label_9 = Label(parent, text="Time:")
+        self.label_9.grid(row=7, column=0, sticky=E)
+        self.entry_7 = Entry(parent, width=8, textvariable=self.total_time)
+        self.entry_7.grid(row=7, column=1)
+        
+        self.label_10 = Label(parent, text="dt:")
+        self.label_10.grid(row=7, column=2, sticky=E)
+        self.entry_8 = Entry(parent, width=8, textvariable=self.dt)
+        self.entry_8.grid(row=7, column=3)
+        
+        self.text_1 = Text(parent, width=55, height=14, wrap=WORD) 
+        self.text_1.grid(rowspan=9 ,row=0, column=4,sticky=S+N)
+        scrl = Scrollbar(parent, command=self.text_1.yview)
+        self.text_1.config(yscrollcommand=scrl.set)
+        scrl.grid(rowspan=9, row=0, column=4, sticky=N+S+E)
+        
+        self.calculate_bt = Button(parent, text="Calculate", command=self.calculate)
+        self.calculate_bt.grid(columnspan=2, row=8, column=0, sticky=N+S+E+W, padx=15, pady=10)
+        
+        self.test_bt = Button(parent, text="Test", command=self.test)
+        self.test_bt.grid(columnspan=2, row=8, column=2, sticky=N+S+E+W, padx=15, pady=10)
+        
         
     def calculate(self):    
         
-        m, beta, k, g, u0, u1 = \
-        self.m.get(), self.beta.get(), self.k.get(), self.g.get(), self.u0.get(), self.u1.get()
+        m, beta, k, g, s_init, v_init, total_time, dt = \
+        self.m.get(), self.beta.get(), self.k.get(), self.g.get(), self.s_init.get(), self.v_init.get(), self.total_time.get(), self.dt.get()
         
-        u_init = [u0, u1]    # initial condition
-        solver = ODESolver(u_init, m, beta, k, g)
-    
-        nperiods = 3     # no of oscillation periods
-        T = 2*pi*nperiods
-        npoints_per_period = 20
-        n = npoints_per_period*nperiods
-        t_points = linspace(0, T, n+1)
-    
-        u, t = solver.solve(t_points)
-    
-        # u is an array of [u0,u1] pairs for each time level,
-        # get the u0 values from u for plotting
-        u0_values = u[:, 0]
-        u1_values = u[:, 1]
-    
-        print '    Second:     \tPosition:\t     Velocity:'
-        for i,j,k in zip(t,u0_values, u1_values):
-            print '%-19.15f %19.15f %19.15f' % (i,j, k)
+        solver = RungeKutta4th(s_init, v_init, m, beta, k, g)
         
-              
+        S, V, T = solver.solve(total_time, dt)
+        
+        N = int(total_time/dt)   #N - Steps to make
+
+        self.text_1.delete('1.0', END)
+        self.text_1.insert('1.0', "Time:\t\t   Stretch(m):      Velocity(m/s):\n")
+        for i in range(N):
+            self.text_1.insert("%d.%d" % (i+2, 0) ,"%13.10f %18.15f %18.15f \n" % (T[i] ,S[i], V[i]))
+
+        plt.plot(T, S, 'r', T, V, 'b', xlabel='time', grid='True', legend=('Stretch(m)', 'Velocity(m/s)'))        
+        plt.show()
+
+
+    def test(self):
+        solver = RungeKutta4th(s_init=1, v_init=0, m=1, beta=0, k=1, g=0)
+        
+        self.m.set(1.)
+        self.beta.set(0.)
+        self.k.set(1.)
+        self.g.set(0.)
+        self.s_init.set(1.)
+        self.v_init.set(0.)
+        
+        print self.total_time.get()
+        if self.total_time.get() == 0:
+            total_time = 30.0
+            self.total_time.set(total_time)
+        else:
+            total_time=self.total_time.get()
+            
+        if self.dt.get() == 0:
+            dt = pi/20
+            self.dt.set(dt)
+        else:
+            total_time=self.total_time.get()
+        dt=self.dt.get()
+ 
+        N = int(total_time/dt)   #N - Steps to make
+        
+        V_error = [0.0]*(N+1)
+        S_error = [0.0]*(N+1)
+        V_exact = [0.0]*(N+1)
+        S_exact = [0.0]*(N+1) 
+        
+        S_solved, V_solved, T = solver.solve(total_time, dt)
+        
+        self.text_1.delete('1.0', END)
+        self.text_1.insert('1.0', "Time:\t\tStretch Error:    Velocity Error:\n")
+        for i in range(N):
+            S_exact[i] = cos(T[i])
+            V_exact[i] = -sin(T[i])
+            
+            S_error[i] = S_exact[i] - S_solved[i]
+            V_error[i] = V_exact[i] - V_solved[i] 
+            
+            self.text_1.insert("%d.%d" % (i+2, 0) ,"%13.10f %18.15f %18.15f \n" % (T[i] ,S_error[i], V_error[i]))
+     
+        plt.figure(1)    
+        plt.plot(T, S_error, 'r', T, V_error, 'b', xlabel='time', grid='True', \
+                 legend=('Stretch(m)', 'Velocity(m/s)'), title=('Error Values'))  
+        
+        plt.figure(2)
+        plt.plot(T, S_solved, 'r', T, V_solved, 'b', xlabel='time', grid='True', \
+                 legend=('Stretch(m)', 'Velocity(m/s)'), title=('RungeKutta-4th Results')) 
+        
+        plt.figure(3)
+        plt.plot(T, S_exact, 'r', T, V_exact, 'b', xlabel='time', grid='True', \
+                 legend=('cos(t)', '-sin(t)'), title=('Exact Values')) 
+        
+     
 root = Tk()
 root.wm_title("Spring Mass")
 gui = Menu(root)
